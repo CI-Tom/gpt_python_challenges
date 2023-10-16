882995d3e5159186dd05d5016e537d8a5514ad1c
tmanaha <L00163060@student.lyit.ie>
2023-10-16 01:59:27 +0100

    Challenge 6 initial commit

--- challenges/6-palindrome_checker.py
+++ challenges/6-palindrome_checker.py
@@ -1,3 +1,4 @@
 print("Welcome to Palindrome Checker!\n")
-input("Please enter a word to check if it is a Palindrome: ")
 
+word = [input("Please enter a word to check if it is a Palindrome: ")]
+print(word)
