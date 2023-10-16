3cc51bc305e876eb3c6031a9cb5d25fd575fb031
tmanaha <L00163060@student.lyit.ie>
2023-10-16 02:03:03 +0100

    Edit list as string

--- challenges/6-palindrome_checker.py
+++ challenges/6-palindrome_checker.py
@@ -1,4 +1,4 @@
 print("Welcome to Palindrome Checker!\n")
 
-word = [input("Please enter a word to check if it is a Palindrome: ")]
+word = [str(input("Please enter a word to check if it is a Palindrome: "))]
 print(word)
