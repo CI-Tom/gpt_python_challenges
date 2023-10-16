74b49afcb6317e051c823608bf8756581a956530
tmanaha <L00163060@student.lyit.ie>
2023-10-16 02:33:56 +0100

    Edit code to reverse string correctly and convert to lower case to avoid errors when matching strings

--- challenges/6-palindrome_checker.py
+++ challenges/6-palindrome_checker.py
@@ -1,4 +1,17 @@
 print("Welcome to Palindrome Checker!\n")
 
-word = [str(input("Please enter a word to check if it is a Palindrome: "))]
-print(word)
+word = input("Please enter a word to check if it is a Palindrome: ")
+# print(word[0], word[1], word[2], word[3])
+
+# Remove spaces and convert to lowercase for case-insensitive comparison
+word = word.replace(" ", "").lower()
+# print(word)
+
+# Create a reversed copy of the word
+reverse_word = word[::-1]
+# print(reverse_word)
+
+if word == reverse_word:
+    print("Congratulations. You found a palindrome!")
+else:
+    print("You failed :( . Please try again.")
